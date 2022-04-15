3f94c4fab3fbbce5cd0fc2fa9aba9f1d70d78a6c
lokanath99 <lokanath1912@gmail.com>
2022-04-15 09:55:16 +0530

    only APIs

--- APIs/Poll/poll.php
+++ APIs/Poll/poll.php
@@ -8,21 +8,48 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once 'C:\xampp\htdocs\FP\authenticate\authDB.php';
 require 'C:\xampp\htdocs\FP\vendor\autoload.php';
 
-
-
 //DB connection
-$db = new dbManager();
-$conn = $db->dbConnect();
+$dataManager = new dbManager();
+$uri = $dataManager->getConnectionString();
 
-$database = $conn->FP;
-$collection = $database->users_of_finanacial_services;
+$client = new MongoDB\Client($uri);
 
-$document = array( 
-    "name"=>'lokanath',
-    "phone"=>'225659625' 
-);
+$changeStream = $client->watch();
 
-$collection->insert($document);
+for ($changeStream->rewind(); true; $changeStream->next()) {
+    if ( ! $changeStream->valid()) {
+        continue;
+    }
 
+    $event = $changeStream->current();
 
+    if ($event['operationType'] === 'invalidate') {
+        break;
+    }
+
+    $ns = sprintf('%s.%s', $event['ns']['db'], $event['ns']['coll']);
+    $id = json_encode($event['documentKey']['_id']);
+
+    switch ($event['operationType']) {
+        case 'delete':
+            printf("Deleted document in %s with _id: %s\n\n", $ns, $id);
+            break;
+
+        case 'insert':
+            printf("Inserted new document in %s\n", $ns);
+            echo json_encode($event['fullDocument']), "\n\n";
+            break;
+
+        case 'replace':
+            printf("Replaced new document in %s with _id: %s\n", $ns, $id);
+            echo json_encode($event['fullDocument']), "\n\n";
+            break;
+
+        case 'update':
+            printf("Updated document in %s with _id: %s\n", $ns, $id);
+            echo json_encode($event['updateDescription']), "\n\n";
+            break;
+    }
+
+}
 ?>
--- authenticate/authDB.php
+++ authenticate/authDB.php
@@ -15,6 +15,9 @@
                 return $e->getMessage();
             }            
         }
+        public function getConnectionString(){
+            return $this->connection_string;
+        }
     }    
         
 
